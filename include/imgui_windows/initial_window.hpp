28a0459b9a2822b37b2ee3bab081505312897667
EnesBaytekin <enesbaytekin38@gmail.com>
2024-11-30 23:55:47 +0300

    chore: add tabs in initial window

--- include/imgui_windows/initial_window.hpp
+++ include/imgui_windows/initial_window.hpp
@@ -3,6 +3,113 @@
 #include "imfilebrowser.h"
 #include <iostream>
 
+void show_tab_item_create_project(AppMain& app) {
+    if (ImGui::BeginTabItem("Create Project")) {
+
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        ImGui::TextWrapped("Create a new project.");
+
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+        ImGui::Separator();
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        static char project_name[128] = "New Dengid Project";
+        
+        static char project_path[256];
+        static bool initialize_project_path = false;
+        if (!initialize_project_path) {
+            strcpy(project_path, std::filesystem::current_path().string().c_str());
+            initialize_project_path = true;
+        }
+        
+        ImGui::Text("Project Name:");
+        ImGui::InputText("##ProjectName", project_name, IM_ARRAYSIZE(project_name));
+        
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        static ImGui::FileBrowser fileDialog(
+            ImGuiFileBrowserFlags_SelectDirectory |
+            ImGuiFileBrowserFlags_CreateNewDir |
+            ImGuiFileBrowserFlags_SkipItemsCausingError
+        );
+
+        ImGui::Text("Project Location:");
+        ImGui::InputText("##ProjectPath", project_path, IM_ARRAYSIZE(project_path),
+            ImGuiInputTextFlags_ReadOnly);
+        ImGui::SameLine();
+        
+        if (ImGui::Button("Browse")) {
+            fileDialog.Open();
+        }
+        fileDialog.Display();
+        if (fileDialog.HasSelected()) {
+            strcpy(project_path, fileDialog.GetSelected().string().c_str());
+            fileDialog.ClearSelected();
+        }
+
+        std::filesystem::path full_path = project_path;
+        full_path.append(project_name);
+        
+        bool file_exists = std::filesystem::exists(full_path);
+        bool name_is_empty = strlen(project_name) == 0;
+        
+        ImGui::Dummy(ImVec2(0.0f, 10.0f));
+
+        if (name_is_empty) {
+            ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "The project name cannot be empty!");
+        } else if (file_exists) {
+            ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "There is already a project with this name!");
+        } else {
+            float line_height = ImGui::GetTextLineHeight();
+            ImGui::Dummy(ImVec2(0.0f, line_height));
+        }
+
+        ImGui::Dummy(ImVec2(0.0f, 10.0f));
+        ImGui::Separator();
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        if (name_is_empty || file_exists) {
+            ImGui::BeginDisabled();
+        }
+
+        ImVec2 window_size = ImGui::GetWindowSize();
+        ImGui::SetCursorPosY(window_size.y-50);
+        if (ImGui::Button("Create", ImVec2(-1, 40))) {
+            std::filesystem::create_directory(full_path);
+        }
+
+        if (name_is_empty || file_exists) {
+            ImGui::EndDisabled();
+        }
+
+        ImGui::EndTabItem();
+    }
+}
+
+void show_tab_item_load_project(AppMain& app) {
+    if (ImGui::BeginTabItem("Load Project")) {
+
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        ImGui::TextWrapped("Load an existing project.");
+
+
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+        ImGui::Separator();
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
+        ImVec2 window_size = ImGui::GetWindowSize();
+        ImGui::SetCursorPosY(window_size.y-50);
+        if (ImGui::Button("Load", ImVec2(-1, 40))) {
+            std::cout << "Loading Project" << std::endl;
+        }
+
+        ImGui::EndTabItem();
+    }
+}
+
+
 void show_initial_window(AppMain& app) {
     int width, height;
     SDL_GetWindowSize(app.window, &width, &height);
@@ -13,63 +120,12 @@ void show_initial_window(AppMain& app) {
         ImGuiWindowFlags_NoMove |
         ImGuiWindowFlags_NoCollapse |
         ImGuiWindowFlags_NoTitleBar
-    );
-    
-    ImGui::TextWrapped("Create a New Project");
+    );    
+    ImGui::BeginTabBar("##initial_window_tab_bar");
 
-    ImGui::Dummy(ImVec2(0.0f, 20.0f));
-    ImGui::Separator();
-    ImGui::Dummy(ImVec2(0.0f, 20.0f));
+    show_tab_item_load_project(app);
+    show_tab_item_create_project(app);
 
-    static char project_name[128] = "New Dengid Project";
-    
-    static char project_path[256];
-    static bool initialize_project_path = false;
-    if (!initialize_project_path) {
-        strcpy(project_path, std::filesystem::current_path().string().c_str());
-        initialize_project_path = true;
-    }
-    
-    ImGui::Text("Project Name:");
-    ImGui::InputText("##ProjectName", project_name, IM_ARRAYSIZE(project_name));
-    
-    ImGui::Dummy(ImVec2(0.0f, 20.0f));
-
-    static ImGui::FileBrowser fileDialog(
-        ImGuiFileBrowserFlags_SelectDirectory |
-        ImGuiFileBrowserFlags_CreateNewDir |
-        ImGuiFileBrowserFlags_SkipItemsCausingError
-    );
-
-    ImGui::Text("Project Location:");
-    ImGui::InputText("##ProjectPath", project_path, IM_ARRAYSIZE(project_path),
-        ImGuiInputTextFlags_ReadOnly);
-    ImGui::SameLine();
-    
-    if (ImGui::Button("Browse")) {
-        fileDialog.Open();
-    }
-    fileDialog.Display();
-    if (fileDialog.HasSelected()) {
-        strcpy(project_path, fileDialog.GetSelected().string().c_str());
-        fileDialog.ClearSelected();
-    }
-
-    ImGui::Dummy(ImVec2(0.0f, 20.0f));
-    ImGui::Separator();
-    ImGui::Dummy(ImVec2(0.0f, 20.0f));
-
-    ImVec2 window_size = ImGui::GetWindowSize();
-    ImGui::SetCursorPosY(window_size.y-50);
-    if (ImGui::Button("Create", ImVec2(-1, 40))) {
-        if (strlen(project_name) > 0 && strlen(project_path) > 0) {
-            std::filesystem::path full_path = project_path;
-            full_path.append(project_name);
-            std::cout << "Creating project: " << full_path << std::endl;
-        } else {
-            std::cout << "Error: Project name or path is empty!" << std::endl;
-        }
-    }
-
+    ImGui::EndTabBar();
     ImGui::End();
 }
