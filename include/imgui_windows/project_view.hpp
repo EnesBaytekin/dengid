b9e7bc1d6391948266c617a87cb6398dc7016ae4
EnesBaytekin <enesbaytekin38@gmail.com>
2024-12-02 21:10:54 +0300

    chore: add close button to settings window

--- include/imgui_windows/project_view.hpp
+++ include/imgui_windows/project_view.hpp
@@ -4,7 +4,7 @@
 #include "app/app_main.hpp"
 #include <SDL2/SDL.h>
 
-void show_settings_window(AppMain& app) {
+void show_settings_window(AppMain& app, bool& settings_window_is_shown) {
     static bool window_is_initialized = false;
     if (!window_is_initialized) {
         int width, height;
@@ -13,7 +13,7 @@ void show_settings_window(AppMain& app) {
         ImGui::SetNextWindowSize(ImVec2(width*0.5f, height*0.5f));
         window_is_initialized = true;
     }
-    ImGui::Begin("Settings", nullptr,
+    ImGui::Begin("Settings", &settings_window_is_shown,
         ImGuiWindowFlags_NoCollapse |
         ImGuiWindowFlags_NoResize
     );
@@ -22,7 +22,10 @@ void show_settings_window(AppMain& app) {
 }
 
 void show_project_view(AppMain& app) {
-    show_settings_window(app);
+    static bool settings_window_is_shown = true;
+    if (settings_window_is_shown) {
+        show_settings_window(app, settings_window_is_shown);
+    }
 }
 
 #endif
