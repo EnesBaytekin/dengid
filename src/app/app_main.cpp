85f44040c53aaf0dc939dab0f0fedafd41cb963b
EnesBaytekin <enesbaytekin38@gmail.com>
2025-04-15 14:51:23 +0300

    chore: add a default rect image to objects

--- src/app/app_main.cpp
+++ src/app/app_main.cpp
@@ -12,6 +12,31 @@ void AppMain::setup() {
     this->add_view(EnumAppViewType::PROJECT_VIEW, get_app_view_project());
 }
 
+std::unique_ptr<Rect> get_ui_rect(Object* object) {
+    if (object->has_component(ComponentType::IMAGE_COMPONENT)) {
+        auto& image_component = *dynamic_cast<ImageComponent*>(object->get_component(ComponentType::IMAGE_COMPONENT).get());
+        return std::make_unique<Rect>(object->position, image_component.get_size());
+    } else {
+        return std::make_unique<Rect>(object->position, 32, 32);
+    }
+}
+
+void update_object_selections() {
+    AppMain& app = AppMain::get_instance();
+    if (app.is_mouse_button_just_pressed(SDL_BUTTON_LEFT)) {
+        Vector2 mouse_position = app.get_mouse_position();
+        for (auto& object : app.get_main_scene()->get_objects()) {
+            auto rect = get_ui_rect(object.get());
+            if (rect->is_colliding(mouse_position)) {
+                ImguiWindowInspector* inspector = dynamic_cast<ImguiWindowInspector*>(app.get_view()->get_window("inspector").get());
+                if (inspector) {
+                    inspector->selected_object = object;
+                }
+            }
+        }
+    }
+}
+
 void AppMain::update() {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -25,21 +50,7 @@ void AppMain::update() {
 
     if (main_scene) {
         main_scene->update();
-        if (is_mouse_button_just_pressed(SDL_BUTTON_LEFT)) {
-            Vector2 mouse_position = get_mouse_position();
-            for (auto& object : main_scene->get_objects()) {
-                if (object->has_component(ComponentType::IMAGE_COMPONENT)) {
-                    auto& image_component = *dynamic_cast<ImageComponent*>(object->get_component(ComponentType::IMAGE_COMPONENT).get());
-                    Rect rect(object->position, image_component.get_size());
-                    if (rect.is_colliding(mouse_position)) {
-                        ImguiWindowInspector* inspector = dynamic_cast<ImguiWindowInspector*>(get_view()->get_window("inspector").get());
-                        if (inspector) {
-                            inspector->selected_object = object;
-                        }
-                    }
-                }
-            }
-        }
+        update_object_selections();
     }
 }
 
--- src/engine/object.cpp
+++ src/engine/object.cpp
@@ -1,6 +1,10 @@
 #include "engine/object.hpp"
 
 void Object::draw() {
+    if (!has_component(ComponentType::IMAGE_COMPONENT)) {
+        AppMain& app = AppMain::get_instance();
+        app.draw_rect(position.x, position.y, 32, 32);
+    }
     for (auto& component : components) {
         component->draw(*this);
     }
